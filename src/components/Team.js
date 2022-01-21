f7530decc3c6962c71f2d36c24695a054bf72ac8
Jeff Lee <jefflee.ldh@gmail.com>
2022-01-21 00:24:27 -0500

    title changed

--- src/components/Team.js
+++ src/components/Team.js
@@ -4,22 +4,14 @@ import Template from './Template';
 const Team = () => {
     return(
         <Template>
-            Ahoy! was started one spring day in 2020 by few crazy high school students attending <a href="https://en.wikipedia.org/wiki/Korean_Minjok_Leadership_Academy">Korean Minjok Leadership Academy.</a> 
+            Ahoy! was started one spring day in 2020 by some high school students from <a href="https://en.wikipedia.org/wiki/Korean_Minjok_Leadership_Academy">Korean Minjok Leadership Academy.</a> 
             <br /><br />
-            <b>Deokhaeng Lee - Co-Founder, PM</b>
+            <b><a href="https://www.linkedin.com/in/dklee0127/">Deokhaeng Lee</a> - Co-Founder, PM</b>
             <br />
             <b>SeongJun Yea - Co-Founder, Developer</b>
             <br />
             <b>James Dong - Co-Founder, Developer</b>
             <br /><br />
-            <b>Minwoo Han - Designer</b>
-            <br />
-            <b>Yoonsang Lee - Supporter</b>
-            <br />
-            <b>Jaewan Kim - Supporter</b>
-            
-
-            
         </Template>
     );
 }
