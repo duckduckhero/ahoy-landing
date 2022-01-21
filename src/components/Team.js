c8d55db9f0d8a2633212bcffe6bd3acf0de5dd11
Jeff Lee <jefflee.ldh@gmail.com>
2022-01-20 21:36:27 -0500

    team changed

--- src/components/Team.js
+++ src/components/Team.js
@@ -6,17 +6,17 @@ const Team = () => {
         <Template>
             Ahoy! was started one spring day in 2020 by few crazy high school students attending <a href="https://en.wikipedia.org/wiki/Korean_Minjok_Leadership_Academy">Korean Minjok Leadership Academy.</a> 
             <br /><br />
-            <b>Deokhaeng Lee - Co-Founder, PM</b>, currently at Duke University.
+            <b>Deokhaeng Lee - Co-Founder, PM</b>
             <br />
-            <b>SeongJun Yea - Co-Founder, Developer</b>, currently at Cooper Union.
+            <b>SeongJun Yea - Co-Founder, Developer</b>
             <br />
-            <b>James Dong - Co-Founder, Developer</b>, currently at Oxford University. 
+            <b>James Dong - Co-Founder, Developer</b>
             <br /><br />
-            <b>Minwoo Han - Designer</b>, currently at Swarthmore College.
+            <b>Minwoo Han - Designer</b>
             <br />
-            <b>Yoonsang Lee - Supporter</b>, currently at Oxford University. 
+            <b>Yoonsang Lee - Supporter</b>
             <br />
-            <b>Jaewan Kim - Supporter</b>, currently at UChicago.
+            <b>Jaewan Kim - Supporter</b>
             
 
             
